85328ec35904e7c89e7bd123d053d4a448be9877
Sudarshan Biswas <sudarshan955@gmail.com>
2016-03-03 16:11:55 +0600

    Set Application name and activity title.

--- app/src/main/java/pw/sudarshanbiswas/app/calculator/MainActivity.java
+++ app/src/main/java/pw/sudarshanbiswas/app/calculator/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle(R.string.home_activity_title);
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
